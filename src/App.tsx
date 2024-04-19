48fb388afcf45bf16196afadb7fb020beaa6668a
Padla Vili <pvilmos2003@gmail.com>
2024-04-19 18:47:11 +0200

    Learn how to use state with TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Component from "./starter/02-props";
+import Component from "./starter/03-state";
 
 function App() {
   return (
     <main>
-      <Component name= "John" id={123}>
-        <h4>This is a children</h4>
-      </Component>
-      <Component name="jdsd" id={323}/>
+      <Component/>
     </main>
   );
 }
--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,8 +1,57 @@
+import { useState } from "react";
+
+
+type Link = {
+  id: number,
+  url: string,
+  text: string
+}
+const navLinks: Link[] = [
+  {
+    id: 1,
+    url: 'some url',
+    text: 'some text'
+  },
+  {
+    id: 2,
+    url: 'some url 2',
+    text: 'some text 2'
+  },
+  {
+    id: 3,
+    url: 'some url 3',
+    text: 'some text 3'
+  },
+]
+
 function Component() {
+
+  const [text, setText] = useState("Something");
+
+  //default the type will be never[], so we need to use generic type definition
+  //const [list, setList] = useState<string[]>([]);
+
+  const [links, setLinks] = useState<Link[]>(navLinks);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+
+      <button onClick={() => {
+        setText("something else");
+
+        //set an array[object]
+
+        setLinks([...links, {
+          id: 4,
+          url: 'some url 4',
+          text: 'some text 4'
+        }])
+
+        console.log(links)
+      }} className="btn btn-center">Click</button>
+
+
+      <p>{text}</p>
     </div>
   );
 }
